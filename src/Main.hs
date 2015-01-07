f9858a8bfae4715a95fa0d7c28a70c0d2813827d
uwap <uwap@nerd-games.com>
2015-01-07 21:10:58 +0100

    Added some more functionality to the bot

--- src/Main.hs
+++ src/Main.hs
@@ -5,19 +5,43 @@ import IRC.Config
 import IRC.Commands
 import IRC.Proto
 
+import System.Random (randomIO)
+import Control.Monad.Reader (liftIO)
+
 main :: IO ()
 main = start $ Config "irc.german-elite.net" 6667 "Sklavin" eventListener
 
 eventListener :: Message -> IRC ()
 eventListener (Message _ (Command "PING") s)         = pong (head s)
 eventListener (Message _ (Command "INVITE") chan)    = mapM_ (joinChannel . Channel) $ tail chan
-eventListener (Message p (Command "PRIVMSG") params) = do
-            case p of
-              Nothing              -> return ()
-              Just (Prefix source) -> do
-                let (Nick nick, _, _) = parseUserHost source
-                case last params of
-                  ('!':'h':'i':_) -> privmsg (Channel $ head params) $ "Hey " ++ nick
-                  _               -> return ()
-                                                
+eventListener (Message p (Command "PRIVMSG") params) = onPrivmsg $ Message p (Command "PRIVMSG") params
 eventListener (Message _ _ _)                        = return ()
+
+onPrivmsg :: Message -> IRC ()
+onPrivmsg (Message p _ params) =
+  case p of
+    Nothing              -> return ()
+    Just (Prefix source) -> do
+      let (n, _, _) = parseUserHost source
+      case last params of
+        ('!':xs) -> onCommand n (Channel $ head params) $ words xs
+        _        -> return ()
+
+onCommand :: Nick -> Channel -> [String] -> IRC ()
+onCommand (Nick nick) (Channel chan) (command:args)
+  | command == "hi"      = privmsg (Channel chan) $ "Hey " ++ nick
+  | command == "penis"   = privmsg (Channel chan) $ "8=========D"
+  | command == "meinung" = meinung args >>= privmsg (Channel chan)
+  | otherwise            = return ()
+
+meinung :: [String] -> IRC String
+meinung args = do
+  let arg = unwords args
+  random <- liftIO randomIO
+  return $ arg ++ " " ++ (meinungen !! (random `mod` length meinungen))
+  
+meinungen :: [String]
+meinungen = [ "finde ich voll toll"
+            , "ist doch mega scheiße"
+            , "interessiert bitte wen?!"
+            ]
