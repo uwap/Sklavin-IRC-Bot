9bef9c03e7debd9b477aa5734726b0bc9c37248f
uwap <uwap@nerd-games.com>
2015-06-03 21:59:44 +0200

    Fix threading

--- src/IRC/Connection.hs
+++ src/IRC/Connection.hs
@@ -17,8 +17,6 @@ import Control.Monad
 import Control.Monad.Reader (asks, liftIO, runReaderT)
 import Control.Concurrent
 
-data ThreadReturn = Restart | Finish
-
 start :: [Message -> IRC ()] -> IO ()
 start eventListeners = do
   conf <- loadConfig
@@ -32,12 +30,11 @@ spawnThreads servers conf eventListeners = do
   where
     spawnThread server = do
       m <- newEmptyMVar
-      _ <- forkFinally (runThread server) $ \eith -> case eith of
-        Left e -> print e >> void (spawnThread server)
-        Right r -> case r of
-                     Restart -> void (spawnThread server)
-                     Finish  -> putMVar m ()
+      _ <- forkThread server m
       return m
+    forkThread server m = forkFinally (runThread server) $ \eith -> case eith of
+      Left e  -> print e >> threadDelay 10000 >> putStrLn ("Reconnecting " ++ server) >> void (forkThread server m)
+      Right _ -> putMVar m ()
     runThread server = do
       irc <- connectTo server conf eventListeners
       runReaderT run irc
@@ -50,21 +47,20 @@ connectTo server conf eventListeners = do
   hSetBuffering h NoBuffering
   return $ Irc h eventListeners conf server
 
-run :: IRC ThreadReturn
+run :: IRC ()
 run = do
   nick <- fromJust <$> lookupServerConfig "nick"
   write $ "NICK " ++ nick
   write $ "USER " ++ nick ++ " 0 * :" ++ nick
   listen
 
-listen :: IRC ThreadReturn
+listen :: IRC ()
 listen = forever $ do
   h <- asks socket
   s <- liftIO $ hGetLine h
   liftIO $ putStrLn s
   eventListeners <- asks listeners
   sequence_ $ eventListeners <*> return (fromRawMessage $ parseCommand s)
-  return Restart
 
 disconnect :: IRC ()
 disconnect = do
