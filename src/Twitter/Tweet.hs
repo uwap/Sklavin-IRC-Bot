a3bdbcd8a718e151ae7fad3aa98b19704cdc40bb
uwap <me@uwap.name>
2015-07-14 18:32:25 +0200

    Make tweets use the real url and not just the t.co shortend link

--- src/Twitter/Tweet.hs
+++ src/Twitter/Tweet.hs
@@ -7,17 +7,29 @@ import GHC.Generics
 import Data.Text
 import Data.Aeson
 
+data TwitterUrl = TwitterUrl { url :: !Text, expanded_url :: !Text } deriving (Generic)
+instance FromJSON TwitterUrl
+instance ToJSON TwitterUrl
+
+data TwitterEntities = TwitterEntities { urls :: ![TwitterUrl] } deriving (Generic)
+instance FromJSON TwitterEntities
+instance ToJSON TwitterEntities
+
 data TwitterUser = TwitterUser { name :: !Text, screen_name :: !Text } deriving (Generic)
 instance FromJSON TwitterUser
 instance ToJSON TwitterUser
 instance Show TwitterUser where
   show user' = unpack (name user') ++ " (@" ++ unpack (screen_name user') ++ ")"
 
-data Tweet = Tweet { text :: !Text, user :: !TwitterUser } deriving (Generic)
+data Tweet = Tweet { text :: !Text, user :: !TwitterUser, entities :: !TwitterEntities } deriving (Generic)
 instance FromJSON Tweet
 instance ToJSON Tweet
 instance Show Tweet where
-  show tweet = show (user tweet) ++ ": " ++ unpack (text tweet)
+  show tweet = show (user tweet) ++ ": " ++ unpack (replaceUrls (urls (entities tweet)) (text tweet))
+
+replaceUrls :: [TwitterUrl] -> Text -> Text
+replaceUrls [] t = t
+replaceUrls (u:us) t = replaceUrls us (replace (url u) (expanded_url u) t)
 
 readTweet :: String -> IRC (Either String Tweet)
 readTweet id' = do
