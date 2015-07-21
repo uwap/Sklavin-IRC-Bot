1b5f3db2ec4883db502d0acf73780d71fc6ff131
uwap <lx1996@web.de>
2015-07-21 11:16:17 +0200

    Fix crash when pasting & into the google command and replace HTML formatting by IRC formatting

--- src/Google/Search.hs
+++ src/Google/Search.hs
@@ -5,6 +5,8 @@ module Google.Search (google) where
 import GHC.Generics
 import Data.Text (Text, unpack)
 import Data.Aeson
+import Data.String.Utils
+import Network.HTTP.Base
 import Network.HTTP.Conduit
 import Control.Concurrent
 import Control.Monad.IO.Class
@@ -29,9 +31,15 @@ google :: MonadIO m => String -> m String
 google q = liftIO $ do
   result <- newEmptyMVar
   _ <- forkIO $ do
-    let url' = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" ++ q
+    let url' = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" ++ urlEncode q
     resp <- simpleHttp url'
     putMVar result $ case eitherDecode resp of
       Left err -> show err
       Right r  -> show $ head $ results r
-  readMVar result
+  replaceFormatting <$> readMVar result
+
+-- The google api returns HTML formatting. This function turns <b> into \x0002
+replaceFormatting :: String -> String
+replaceFormatting = flip replaceAll [("<b>","\x0002"), ("</b>","\x000F")]
+  where
+    replaceAll = foldl (flip (uncurry replace)) 
